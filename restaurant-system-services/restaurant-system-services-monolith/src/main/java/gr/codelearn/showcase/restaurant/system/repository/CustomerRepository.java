3f25f80251d1c3b38bb4ddbdf71a34a039e12cbb
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2025-10-31 15:41:22 +0200

    Added @Repository annotation for clarity and documentation purposes.

--- restaurant-system-services/restaurant-system-services-monolith/src/main/java/gr/codelearn/showcase/restaurant/system/repository/CustomerRepository.java
+++ restaurant-system-services/restaurant-system-services-monolith/src/main/java/gr/codelearn/showcase/restaurant/system/repository/CustomerRepository.java
@@ -2,9 +2,11 @@ package gr.codelearn.showcase.restaurant.system.repository;
 
 import gr.codelearn.showcase.restaurant.system.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 	Optional<Customer> findByNameAndEmail(String name, String email);
 }
--- restaurant-system-services/restaurant-system-services-monolith/src/main/java/gr/codelearn/showcase/restaurant/system/repository/ReservationRepository.java
+++ restaurant-system-services/restaurant-system-services-monolith/src/main/java/gr/codelearn/showcase/restaurant/system/repository/ReservationRepository.java
@@ -2,6 +2,8 @@ package gr.codelearn.showcase.restaurant.system.repository;
 
 import gr.codelearn.showcase.restaurant.system.domain.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 }
