1e4b80de9347628f9ac9133c89c8304b81839b03
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2025-11-21 15:50:38 +0200

    Added validations in customer creation
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- restaurant-system-services/restaurant-system-services-monolith/src/main/java/gr/codelearn/showcase/restaurant/system/api/resource/CustomerResource.java
+++ restaurant-system-services/restaurant-system-services-monolith/src/main/java/gr/codelearn/showcase/restaurant/system/api/resource/CustomerResource.java
@@ -1,4 +1,7 @@
 package gr.codelearn.showcase.restaurant.system.api.resource;
 
-public record CustomerResource(Long id, String name, String phone, String email) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
+public record CustomerResource(Long id, @NotEmpty String name, @NotEmpty String phone, @Email String email) {
 }
