5f42f1fed35aa1573e46e08754c2998c3e39679e
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2025-12-18 12:37:35 +0200

    Initial introduction of Config Server.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

---
+++ restaurant-system-infra/restaurant-system-config-server/src/main/java/gr/codelearn/showcase/infra/config/config/ConfigServerConfig.java
@@ -0,0 +1,9 @@
+package gr.codelearn.showcase.infra.config.config;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigServer
+public class ConfigServerConfig {
+}
